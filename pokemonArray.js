0b98046cbc7cadbc44d0083cbc9459febb04017f
MugURCode <Jer50580@gmail.com>
2017-07-11 12:01:06 -0400

    first commit

---
+++ pokemonArray.js
@@ -0,0 +1,27 @@
+/* Array called Pokemon Roster
+Make two variables 1st and 2nd to fight
+
+*/
+
+var pokemonRoster = ["Empoleon", "Charizard", "Piplup", "Dialga", "Pikachu", "Snorlax"];
+var firstFight = pokemonRoster[0],
+secondFight = pokemonRoster[1];
+
+for (i=0; i<6; i++) {
+    console.log(pokemonRoster[i]);
+}
+
+
+/*nameArray = ["bob", "mahdi", "cyril", "juan", "mike"];
+
+
+for (i=0; i<5; i++) {
+    console.log(nameArray[i]);
+}
+
+*/
+
+
+
+
+
---
+++ pokemonVariable.js
@@ -0,0 +1,14 @@
+var empoleonHP = 278;
+var empoleonNum = 395;
+var empoleonType1 = "Water";
+var empoleonType2 = "Steel";
+var empoleonBestMove= "Waterfall";
+var empoleonLastEvo = true;
+var empoleonAttack = 86;
+var empoleonDef = 88;
+var empoleonspecialAtt= 111;
+var empoleonSpeed = 101;
+
+
+empoleonHP = empoleonHP -10; 
+
